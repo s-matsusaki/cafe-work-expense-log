dc2da93f9f93771a361a06c34feeb21fc355d728
s-matsusaki <f.cesca.zkmt@gmail.com>
2026-05-20 09:26:03 +0900

    tailwind導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>@yield('title', 'カフェログ')</title>
 </head>
-<body>
+<body class="bg-slate-100 text-slate-900">
     <header>
         <h1>カフェログ</h1>
 
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,18 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        // docker環境で127.0.0.1を指定してもnodeコンテナ自身を指すので接続できない
+        // Docker環境では外部から接続できるように全IPで待ち受ける
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+
+        // ブラウザ側・HMR側では localhost を使わせる
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
